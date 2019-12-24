0e35ea24f81f948034027cd0f22954088c15ac20
Mr <37304121+sbondo1234@users.noreply.github.com>
2019-12-24 23:50:45 +0000

    alertEm for cleaning

--- sbondPokem.js
+++ sbondPokem.js
@@ -7,23 +7,43 @@ registerPlugin({
     autorun: true
 }, function(sinusbot, config){
     var event = require('event');
+	var backend = require('backend');
+	var engine = require('engine');
 
 	event.on('chat', function(ev){
-		var backend = require('backend');
-		var engine = require('engine');
 		var client = backend.getClientByName(ev.client.name());
 
 		engine.log('Got message "'+ev.text +'" from '+ ev.client.name());
 		
 		if(ev.text.indexOf("!poke") > -1 && client.databaseID() == "7"){
-			client.chat(ev.text);
-			
-			var removeCommand = ev.text.replace('!poke ','');
-			var user = backend.getClientByName(removeCommand);
-			
-			for ( var i = 0; i < 10; i++ ) {
-				user.poke('Hello, ' + user.name());
+			alertEm("poke", ev.text);
+		}
+		
+		if(ev.text.indexOf("!chat") > -1){
+			alertEm("chat", ev.text);
+		}
+		
+		function alertEm(which, command){
+			if(which == "poke"){
+				var removeCommand = ev.text.replace('!poke ','');
+				var user = backend.getClientByName(removeCommand);
+				
+				for(var i = 0; i < 10; i++){
+					user.poke('Hello, ' + user.name());
+				}
+			}
+			else if(which == "chat"){
+				var removeCommand = ev.text.replace('!chat ','');
+				var user = backend.getClientByName(removeCommand);
+				
+				for(var i = 0; i < 10; i++){
+					user.chat('Hello, ' + user.name());
+				}
+			}
+			else{
+				
 			}
 		}
 	})
+		
 });
